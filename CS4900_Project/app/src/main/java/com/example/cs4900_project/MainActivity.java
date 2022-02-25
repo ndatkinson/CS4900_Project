cd6be2a23a5b644389f6714f272da58123c89d15
ndatkinson <89750966+ndatkinson@users.noreply.github.com>
2022-02-25 12:56:30 -0500

    Added External Permissions

--- CS4900_Project/app/src/main/java/com/example/cs4900_project/MainActivity.java
+++ CS4900_Project/app/src/main/java/com/example/cs4900_project/MainActivity.java
@@ -2,12 +2,16 @@ package com.example.cs4900_project;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.os.Build;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            requestPermissions(new String[]
+                    {android.Manifest.permission.READ_EXTERNAL_STORAGE}, 1);
+        }
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
