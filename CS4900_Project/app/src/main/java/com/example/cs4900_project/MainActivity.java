e205f8fc2700dd1530cad4d3a54e44abe5894c85
jellis56 <je179086@gmail.com>
2022-03-13 22:50:13 -0400

    Android Studio Initial Setup
    Created the UI for the app in Android Studio. User is able to pick an image from storage. Still need to figure out how to load pytorch model in Android Studio.

--- CS4900_Project/app/src/main/java/com/example/cs4900_project/MainActivity.java
+++ CS4900_Project/app/src/main/java/com/example/cs4900_project/MainActivity.java
@@ -2,10 +2,22 @@ package com.example.cs4900_project;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.media.Image;
+import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity {
+    Bitmap bitmap = null;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
@@ -14,5 +26,57 @@ public class MainActivity extends AppCompatActivity {
         }
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+
+        Button buttonLoad = findViewById(R.id.btnLoad);
+        //button to load image into the ImageView
+        buttonLoad.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent intent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                startActivityForResult(intent, 3);
+            }
+        });
+
+        Button buttonSegment = findViewById(R.id.btnSegment);
+        //button to start the process of segmenting the image
+        buttonSegment.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                segment();
+
+            }
+        });
+
+
     }
+
+    //this method handles the selection of the image from the emulator storage and displays it in the imageView
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (resultCode == RESULT_OK && data != null) {
+            Uri selectedImage = data.getData();
+            ImageView imageView = findViewById(R.id.imageView);
+            try {
+                bitmap = MediaStore.Images.Media.getBitmap(getApplicationContext().getContentResolver(), selectedImage);
+                imageView.setImageBitmap(bitmap);
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+
+    }
+
+    //this method handles the segmentation of the image
+    protected void segment(){
+        
+    }
+
+
 }
