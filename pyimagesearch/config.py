78f87b5400e78cdc2324609ab3154633643b73f4
AlbusAndreus <39923632+AlbusAndreus@users.noreply.github.com>
2022-03-14 23:56:16 -0400

    Files for the script to Produce model
    Script is almost working. The script starts training the model, but stops because previous process has not finished. Someone take a look at this and see what you can come up with. Missing Dataset for file size limit issue

---
+++ pyimagesearch/config.py
@@ -0,0 +1,33 @@
+import torch
+import os
+
+DATASET_PATH = os.path.join("Dataset", "VOC2012")
+
+IMAGE_DATASET_PATH = os.path.join(DATASET_PATH, "SegmentationObject")
+MASK_DATASET_PATH = os.path.join(DATASET_PATH, "SegmentationClass")
+
+TEST_SPLIT = 0.20
+
+DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
+
+PIN_MEMORY = True if DEVICE == "cuda" else False
+
+NUM_CHANNELS = 1
+NUM_CLASSES = 1
+NUM_LEVELS = 3
+
+INIT_LR = 0.001
+NUM_EPOCHS = 35
+BATCH_SIZE = 64
+
+INPUT_IMAGE_WIDTH = 400
+INPUT_IMAGE_HEIGHT = 300
+
+THRESHOLD = 0.5
+
+BASE_OUTPUT = "output"
+
+MODEL_PATH = os.path.join(BASE_OUTPUT, "unit_tgs_VOC2012")
+PLOT_PATH = os.path.sep.join([BASE_OUTPUT, "plot.png"])
+TEST_PATHS = os.path.sep.join([BASE_OUTPUT, "test_pths.txt"])
+
